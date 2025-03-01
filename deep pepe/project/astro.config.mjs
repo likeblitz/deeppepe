b9391d12228477d92a01bff6e1c1b61da649c10b
likeblitz <bsu.miana@gmail.com>
2025-03-01 22:00:57 +0330

    Delete deep pepe directory

--- deep pepe/project/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
-export default defineConfig({
-  integrations: [tailwind(), react()],
-});
